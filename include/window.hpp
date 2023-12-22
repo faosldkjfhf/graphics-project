03bcf4dee4c39ac046dbf1631c980e94176be2fc
Kevin Liu <kliu0932@gmail.com>
2023-12-22 00:14:09 -0500

    bear -- make to fix lsp issues

--- include/window.hpp
+++ include/window.hpp
@@ -2,13 +2,19 @@
 #define WINDOW
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
 
 /*
  * Creates a window for the program
  */
 class Window {
 public:
+  Window();
+  ~Window();
+  SDL_Window *GetSDLWindow();
+
 private:
+  SDL_Window *window;
 };
 
 #endif
--- src/window.cpp
+++ src/window.cpp
@@ -1,1 +1,7 @@
 #include "window.hpp"
+
+Window::Window() {}
+
+Window::~Window() {}
+
+SDL_Window *Window::GetSDLWindow() { return nullptr; }
